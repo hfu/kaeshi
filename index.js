671e4521862771a6dbc3df2b454f4c07b4a06ae3
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-08-30 16:41:33 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const config = require('config')
+const fs = require('fs')
+const proxy = require('http-proxy').createServer({
+  ssl: {
+    key: fs.readFileSync(config.get('key')),
+    cert: fs.readFileSync(config.get('cert'))
+  },
+  target: 'https://basemaps.arcgis.com',
+  secure: true
+})
