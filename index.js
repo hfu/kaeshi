fc4db170fa98c973aebc73a81c7d24530400f843
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-08-30 18:10:02 -0400

    update

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const config = require('config')
 const fs = require('fs')
-// const https = require('https')
-const httpProxy = require('http-proxy')
+const https = require('https')
+const HttpProxy = require('http-proxy')
 // const proxy = new HttpProxy({ changeOrigin: true})
 
-httpProxy.createProxyServer({
-  changeOrigin: true, 
-  target: config.get('target')
-}).listen(config.get('port'))
+const proxy = new HttpProxy({
+  changeOrigin: true//,
+  //target: config.get('target')
+})
+
+https.createServer(
+  {
+    ssl: {
+      key: fs.readFileSync(config.get('key')),
+      cert: fs.readFileSync(config.get('cert'))
+    }
+  },
+  (req, res) => {
+    proxy.web(req, res, { target: config.get('target') })
+  } 
+).listen(config.get('port'))
 
 /*
 https.createServer(
