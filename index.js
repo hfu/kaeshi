e7c2abec845f7bf13f5e4cbe62888f926fea0230
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-08-31 20:09:45 +0200

    update

--- index.js
+++ index.js
@@ -4,11 +4,37 @@ const tls = require('tls')
 const https = require('https')
 const HttpProxy = require('http-proxy')
 const proxy = new HttpProxy({ changeOrigin: true })
+const modifyResponse = require('node-http-proxy-json')
 
 const context = tls.createSecureContext({
   key: fs.readFileSync(config.get('key')),
   cert: fs.readFileSync(config.get('cert')),
   ca: fs.readFileSync(config.get('ca'))
+})
+
+proxy.on('proxyRes', (proxyRes, req, res) => {
+console.log(req.url)
+console.log(proxyRes.headers)
+console.log('---')
+return
+  modifyResponse(res, proxyRes, (body) => {
+    if (body) {
+      body.age = 1
+    }
+    return body
+  })
+/*
+  let body = []
+  proxyRes.on('data', chunk => {
+    body.push(chunk)
+  })
+  proxyRes.on('end', () => {
+    body = Buffer.concat(body).toString()
+    console.log(req.url)
+    // console.log(body.toString())
+    // res.end()
+  })
+*/
 })
 
 https.createServer(
@@ -25,6 +51,9 @@ https.createServer(
   },
   (req, res) => {
     console.log(req.url)
-    proxy.web(req, res, { target: config.get('target') })
+    proxy.web(req, res, {
+      target: config.get('target')//,
+      //selfHandleResponse: true
+    })
   }
 ).listen(config.get('port'))
