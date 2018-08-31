c79afae80e2f82dff6c4ff6ce0f4a2249af60f16
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-08-31 23:30:50 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const config = require('config')
 const fs = require('fs')
+const zlib = require('zlib')
 const tls = require('tls')
 const https = require('https')
 const HttpProxy = require('http-proxy')
 const proxy = new HttpProxy({ changeOrigin: true })
-const modifyResponse = require('node-http-proxy-json')
+//const modifyResponse = require('node-http-proxy-json')
 
 const context = tls.createSecureContext({
   key: fs.readFileSync(config.get('key')),
@@ -12,21 +13,40 @@ const context = tls.createSecureContext({
   ca: fs.readFileSync(config.get('ca'))
 })
 
-proxy.on('proxyRes', (proxyRes, req, res) => {
-  if (req.url.endsWith('pbf') || req.url.endsWith('ico')) return
+proxy.on('XXproxyRes', (proxyRes, req, res) => {
+  let body = []
   console.log(req.url)
   console.log(proxyRes.headers)
-  modifyResponse(res, proxyRes, (body) => {
-console.log('BODYBODYBODY')
-    if (body) {
-console.log('*')
-      console.log(body)
-      body.age = 1
-    }
-    console.log(body)
-    console.log('---')
-    return body
+  proxyRes.on('data', data => {
+    body.push(data)
   })
+  proxyRes.on('end', () => {
+    body = Buffer.concat(body)
+    if (proxyRes.headers['content-encoding'] === 'gzip') {
+      body = zlib.gunzipSync(body)
+    }
+    if (!req.url.endsWith('pbf')) console.log(body.toString('utf-8'))
+
+
+
+    // NG body = zlib.gzipSync(body)
+    res.headers = proxyRes.headers
+    res.headers['content-length'] = Buffer.byteLength(body)
+    res.end(body)
+    console.log(res.headers)
+    console.log('===')
+  })
+/*
+  if (req.url.endsWith('pbf') || req.url.endsWith('ico')) return
+  const _end = res.end, _writeHead = res.writeHead, _write = res.write
+  let chunks
+  proxyRes.on('data', chunk => {
+    console.log(chunk.toString('utf-8'))
+  })
+  proxyRes.on('end', () => {
+    console.log('end')
+  })
+*/
 })
 
 https.createServer(
@@ -43,7 +63,8 @@ https.createServer(
   },
   (req, res) => {
     proxy.web(req, res, {
-      target: config.get('target')
+      target: config.get('target'), 
+      selfHandleResponse: false
     })
   }
 ).listen(config.get('port'))
